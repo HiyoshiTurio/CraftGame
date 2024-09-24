f39645d625be963d2464ca9bfb9fdfd6715260d8
HiyoshiTurio <turio01217@gmail.com>
2024-09-24 22:57:11 +0900

    アイテムの詳細テキストの追加

--- Assets/Scripts/ItemIconBase.cs
+++ Assets/Scripts/ItemIconBase.cs
@@ -3,15 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class ItemIconBase : MonoBehaviour , IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] private Text detailText;
+    [SerializeField] private GameObject detailPanel;
+
     public void SetItemID(ItemType itemType)
     {
         int itemID = (int)itemType;
         GetComponent<Image>().sprite = DataManager.Instance.IconData.itemDatas[itemID].icon;
+        detailText.text = DataManager.Instance.IconData.itemDatas[itemID].itemType.ToString();
+        detailPanel.gameObject.SetActive(false);
     }
-    public void OnPointerEnter(PointerEventData eventData){} //カーソルがアイコン内に入ったとき{ }
-    public void OnPointerExit(PointerEventData eventData){} //カーソルがアイコンから出たとき{ }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        detailPanel.gameObject.SetActive(true);
+    } //カーソルがアイコン内に入ったとき{ }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        detailPanel.gameObject.SetActive(false);
+    } //カーソルがアイコンから出たとき{ }
 }
